2061174a4605378d79218090690ed5cf858231a5
aaron <aaronbaon1@gmail.com>
2024-10-21 16:23:37 +0800

    change import

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let repository: Repository<User>;
 
   // Mock the repository methods
